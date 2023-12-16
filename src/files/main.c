bfe30bbeaf26bb5a5e1f84b652dca46e0326325c
Mohamed Nezhari <mohamednezxx@gmail.com>
2023-12-16 17:59:35 +0100

    errors fix
    changed some syntax errors

--- src/files/main.c
+++ src/files/main.c
@@ -23,17 +23,17 @@ int i;
 for(i=0;i<N;i++){
 
 printf("Le Nom du Medicament est %s ,Valable:%u",(med+i)->nom,(med+i)->quant_stock);
+}
 
 }
-medicament* init(int N;medicament *med){
+
+medicament* init(int n,medicament *med){
     int i;
-    for(i=0;i<N;i++){
+    for(i=0;i<n;i++){
        printf("Entrer le nom et la qt");
        scanf("%s%u",(med+i)->nom,&(med+i)->quant_stock);
     }
-}
-return med;
-
+    return med;
 }
 
 
