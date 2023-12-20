f3d7164b00cb319cf93b44d1ff6df69178593b45
Mohamed Nezhari <mohamednezxx@gmail.com>
2023-12-20 20:07:20 +0100

    case 3 done

--- src/files/main.c
+++ src/files/main.c
@@ -7,26 +7,89 @@ typedef struct {
 int code;
 char *nom,*adresse,*nationalite;
 
-}fournissuer;
+}fournisseur;
 
 //structure d'un medicament
 typedef struct{
     char *nom,*code,*categorie;
     float prix;
    unsigned int quant_stock;
-    fournissuer forn;
+    fournisseur forn;
 
 }medicament;
-
+//fontion d'affichage des cas possible
 void affichage(int a,int N,medicament *med){
-int i;
+int i,somme_med_valable=0;
+//
 for(i=0;i<N;i++){
+switch (a)
+{
+case 1:
+//premier cas si l'utilisateur entre la valeur 1 
+for(i=0;i<N;i++){
+    printf("Medicament : %s =>Valable en stock:%u\n",(med+i)->nom,(med+i)->quant_stock);
+somme_med_valable+=(med+i)->quant_stock;
+}
+printf("La somme des medicaments valable est:%d",somme_med_valable);
+    break;
+case 2:
 
-printf("Le Nom du Medicament est %s ,Valable:%u",(med+i)->nom,(med+i)->quant_stock);
+
+break;
+case 3:
+char c;
+printf("Entrer :\na-D'apres le fournisseur donnee\nb-D'apres ca categorie donnee\nc-D'apres le prix\n");
+scanf("%c",&c);
+switch (c)
+{
+case 'a':
+char *s;
+s=(char*)malloc(10*sizeof(char));
+printf("Entrer le nom du fournisseur:\n");
+    scanf("%s",s);
+    for(i=0;i<N;i++){
+        if(s==(med+i)->forn.nom){
+            printf("Medicament:%s\tCode:%s\tCategorie:%s\tValable en stock:%u\tPrix:%.2f\n",(med+i)->nom,(med+i)->code,(med+i)->categorie,(med+i)->quant_stock,(med+i)->prix);
+        }
+    }
+    free(s);
+    break;
+case 'b':
+char *s;
+s=(char*)malloc(10*sizeof(char));
+    printf("Entrer la categorie:\n");
+    scanf("%s",s);
+     for(i=0;i<N;i++){
+        if(s==(med+i)->categorie){
+            printf("Medicament:%s\tCode:%s\tCategorie:%s\tValable en stock:%u\tPrix:%.2f\n",(med+i)->nom,(med+i)->code,(med+i)->categorie,(med+i)->quant_stock,(med+i)->prix);
+        }
+    }
+    free(s);
+    break;
+    case 'c':
+    float pr;
+          printf("Entrer le prix:\n");
+            scanf("%f",&pr);
+     for(i=0;i<N;i++){
+        if(abs(pr-(med+i)->prix) < 1e-9){
+            printf("Medicament:%s\tCode:%s\tCategorie:%s\tValable en stock:%u\tPrix:%.2f\n",(med+i)->nom,(med+i)->code,(med+i)->categorie,(med+i)->quant_stock,(med+i)->prix);
+        }}
+    break;
+default:
+printf("Error");
+    break;
 }
 
+
+break;
+default:
+    break;
+    }
 }
 
+
+}
+//fontion d'initialisation qui retourne la premiere adresse du tableau de type med(medicaments)
 medicament* init(int n,medicament *med){
     int i;
     for(i=0;i<n;i++){
