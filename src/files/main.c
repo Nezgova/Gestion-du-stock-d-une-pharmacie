9c314766644bfd16fc61c2e3592a87a8b10a2f8a
Mohamed Nezhari <mohamednezxx@gmail.com>
2023-12-22 20:19:02 +0100

    update
    addeed case 4 and 5 and finished 3

--- src/files/main.c
+++ src/files/main.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void init(int n,medicament *med);
-void affichage(int a,int N,medicament *med);
+
 //structure d'un fournisseur
 typedef struct {
 int code;
@@ -18,18 +17,22 @@ typedef struct{
     fournisseur forn;
 
 }medicament;
-
+void init(int n,medicament *med);
+void affichage(int a,int *N,medicament *med);
 
 //fontion d'affichage des cas possible
-void affichage(int a,int N,medicament *med){
-int i;
-for(i=0;i<N;i++){
+void affichage(int a,int *N,medicament *med){
+int i,j;
+char c;
+char *s;
+medicament temp;
+s=(char*)malloc(10*sizeof(char));
 switch (a)
 {
 case 1:
 //premier cas si l'utilisateur entre la valeur 1
  int somme_med_valable=0;
-for(i=0;i<N;i++){
+for(i=0;i<*N;i++){
     printf("Medicament : %s =>Valable en stock:%u\n",(med+i)->nom,(med+i)->quant_stock);
 somme_med_valable+=(med+i)->quant_stock;
 }
@@ -41,18 +44,15 @@ case 2:
 break;
 case 3:
 //3eme cas si l'utilisateur entre le nombre 3
-char c;
 printf("Entrer :\na-D'apres le fournisseur donnee\nb-D'apres ca categorie donnee\nc-D'apres le prix\n");
 scanf("%c",&c);
 switch (c)
 {
 case 'a':
 //cas d'affichage de tous lesmedicaments d'apres un fournisseur
-char *s;
-s=(char*)malloc(10*sizeof(char));
 printf("Entrer le nom du fournisseur:\n");
     scanf("%s",s);
-    for(i=0;i<N;i++){
+    for(i=0;i<*N;i++){
         if(s==(med+i)->forn.nom){
             printf("Medicament:%s\tCode:%s\tCategorie:%s\tValable en stock:%u\tPrix:%.2f\n",(med+i)->nom,(med+i)->code,(med+i)->categorie,(med+i)->quant_stock,(med+i)->prix);
         }
@@ -61,11 +61,9 @@ printf("Entrer le nom du fournisseur:\n");
     break;
 case 'b':
 //cas d'affichage de tous lesmedicaments d'apres la categorie
-char *s;
-s=(char*)malloc(10*sizeof(char));
     printf("Entrer la categorie:\n");
     scanf("%s",s);
-     for(i=0;i<N;i++){
+     for(i=0;i<*N;i++){
         if(s==(med+i)->categorie){
             printf("Medicament:%s\tCode:%s\tCategorie:%s\tValable en stock:%u\tPrix:%.2f\n",(med+i)->nom,(med+i)->code,(med+i)->categorie,(med+i)->quant_stock,(med+i)->prix);
         }
@@ -77,7 +75,7 @@ s=(char*)malloc(10*sizeof(char));
     float pr;
           printf("Entrer le prix:\n");
             scanf("%f",&pr);
-     for(i=0;i<N;i++){
+     for(i=0;i<*N;i++){
         if(abs(pr-(med+i)->prix) < 1e-9){
             printf("Medicament:%s\tCode:%s\tCategorie:%s\tValable en stock:%u\tPrix:%.2f\n",(med+i)->nom,(med+i)->code,(med+i)->categorie,(med+i)->quant_stock,(med+i)->prix);
         }}
@@ -88,21 +86,105 @@ printf("Error");
 }
 break;
 case 4:
-//cas d'ajout d'un medicament
+//4eme cas d'ajout d'un medicament
 medicament med1;
-init(1,med1);
-char c;
+init(1,&med1);
 printf("Entrer :\na-Au debut du tableau\nb-A la fin du tableau\nc-En une position a votre choix\nd-Apres le 2eme medicament de meme categorie\n");
 scanf("%c",&c);
 switch (c)
 {
 case 'a':
-
-   
-   
-   
+med=(medicament*)realloc(med,(*N+1)*sizeof(int));
+for(i=*N;i>0;i--){
+*(med+i)=*(med+i-1);
+}
+*med=med1;
+*N=(*N)+1;
     break;
+    case 'b':
+med=(medicament*)realloc(med,(*N+1)*sizeof(int));
+*(med+*N)=med1;
+*N=(*N)+1;
+    break;
+    case 'c':
+printf("Veuiller donner la position ou vous voulez ajouter votre nouveau medicament:\n");
+scanf("%d",&j);
+med=(medicament*)realloc(med,(*N+1)*sizeof(int));
+for(i=*N;i>j;i++){
+*(med+i)=*(med+i-1);
+}
+*(med+j)=med1;
+*N=(*N)+1;
+    break;
+    case 'd':
+    char cat[10];
+    printf("Veuiller Entrer La Categorie:");
+    scanf("%s",cat);
+    med=(medicament*)realloc(med,(*N+1)*sizeof(int));
+    for(i=0;i<*N;i++){
+if(strcmp(cat,(med+i)->categorie)==0){
+    j++;
+}
+if(j==2){
+    break;
+}
+    }
+    j=i+1;
+    for(i=*N;i>j;i++){
+*(med+i)=*(med+i-1);
+    }
+    *(med+j)=med1;
+*N=(*N)+1;;
+    break;
+default:
+printf("Error");
+    break;
+}
+break;
+case 5:
+//5eme cas de supression d'un medicament
+printf("Entrer :\na-Au debut du tableau\nb-A la fin du tableau\nc-En une position a votre choix\nd-Par Nom Du Medicament\ne-Par fournisseur\nf-Dont nom du fournisseur termine par une lettre donne:\n");
+scanf("%c",&c);
+switch (c)
+{
+case 'a':
+temp=*(med+*N);
+*(med+*N)=*med;
+*med=temp;
+med=(medicament*)realloc(med,(*N-1)*sizeof(medicament));
+    break;
+    *N=(*N)-1;
+case 'b':
+med=(medicament*)realloc(med,(*N-1)*sizeof(medicament));
+*N=(*N)-1;
+break;
+case 'c':
+printf("Entrer votre position:\n");
+scanf("%d",&j);
+temp=*(med+*N);
+*(med+*N)=*(med+j);
+*(med+j)=temp;
+med=(medicament*)realloc(med,(*N-1)*sizeof(medicament));
+*N=(*N)-1;
+break;
+case 'd':
+printf("Entrer le nom du medicament:");
+scanf("%s",s);
+for(i=0;i<*N;i++){
+    if(strcmp((med+i)->nom,s)==0){
+        j=i;
+        break;
+    }
+}
+temp=*(med+*N);
+*(med+*N)=*(med+j);
+*(med+j)=temp;
+med=(medicament*)realloc(med,(*N-1)*sizeof(medicament));
+*N=(*N)-1;
+break;
+case 'e':
 
+break;
 default:
     break;
 }
@@ -110,11 +192,11 @@ break;
 default:
     break;
     }
-}
 
 
+
 }
-//fontion d'initialisation qui retourne la premiere adresse du tableau de type med(medicaments)
+//fontion d'initialisation
 void init(int n,medicament *med){
     int i;
     for(i=0;i<n;i++){
@@ -133,20 +215,8 @@ printf("Menu De Gestion\n");
 medicament *t;
 t=(medicament*)malloc(N*sizeof(medicament));
 init(N,t);
-affichage(1,N,t);
+affichage(1,&N,t);
 
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
